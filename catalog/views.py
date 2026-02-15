aa7066dcc36b93dacbb7b93daaaa6ac739789e7a
Tony Peterson <peterson.anthonyl@gmail.com>
2026-02-14 19:24:36 -0500

    Initial entry of the Django DRF and first REST url for products.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
