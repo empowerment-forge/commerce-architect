369760c92af3355bfa1602521d200229224bf5b4
Tony Peterson <peterson.anthonyl@gmail.com>
2026-02-15 02:45:29 -0500

    Implement JWT auth domain with registration and protected products API

---
+++ accounts/views.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+            },
+            status=status.HTTP_201_CREATED,
+        )
